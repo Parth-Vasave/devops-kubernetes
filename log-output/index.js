37addacbfcef5c5c651cc2064f453ee46a27ccc6
parth <mailparthvasave@gmail.com>
2026-09-11 00:11:25 +0530

    Complete exercise 1.7

--- log-output/index.js
+++ log-output/index.js
@@ -1,4 +1,9 @@
 const crypto = require("crypto");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 const randomString = crypto.randomBytes(8).toString("hex");
 
@@ -6,3 +11,12 @@ setInterval(() => {
     const timestamp = new Date().toISOString();
     console.log(`${timestamp} ${randomString}`);
 }, 5000);
+
+app.get("/", (req, res) => {
+    const timestamp = new Date().toISOString();
+    res.send(`${timestamp} ${randomString}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started in port ${PORT}`);
+});

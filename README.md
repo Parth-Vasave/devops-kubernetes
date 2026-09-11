94bb6490fa8ad636f96cd57f812a0896fae6558d
parth <mailparthvasave@gmail.com>
2026-09-11 14:53:19 +0530

    Complete exercise 1.9

--- README.md
+++ README.md
@@ -16,3 +16,5 @@
 - [1.7.](https://github.com/Parth-Vasave/devops-kubernetes/tree/1.7/log-output)
 
 - [1.8.](https://github.com/Parth-Vasave/devops-kubernetes/tree/1.8/todo-app)
+
+- [1.9.](https://github.com/Parth-Vasave/devops-kubernetes/tree/1.9/ping-pong)
---
+++ ping-pong/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+let counter = 0;
+
+app.get("/pingpong", (req, res) => {
+  res.send(`pong ${counter}`);
+  counter++;
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
